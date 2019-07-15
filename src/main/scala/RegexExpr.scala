1dd897400247fb22aae58307767858a268998aee
John Yang <john.yang20@berkeley.edu>
2019-07-15 16:42:30 -0700

    Create initial list of regex expression classes

---
+++ src/main/scala/RegexExpr.scala
@@ -0,0 +1,9 @@
+/*
+ * Part 1: Define the hierarchy classes for constructing an abstract syntax tree.
+ */
+abstract class RegexExpr
+case class Literal(c: Char) extends RegexExpr // '.', 'a'
+case class Or(expr1: RegexExpr, expr2: RegexExpr) extends RegexExpr // a|b
+case class Concat(first: RegexExpr, second: RegexExpr) // ab => Concat(a,b); abc => Concat(a, Concat(b, c))
+case class Repeat(expr: RegexExpr) extends RegexExpr
+case class Plus(expr: RegexExpr) extends RegexExpr
