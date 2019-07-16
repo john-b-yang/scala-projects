c7f8c85029d17416a8e4b503b2cec1b45661cc63
John Yang <john.yang20@berkeley.edu>
2019-07-16 12:59:50 -0700

    added testing folder, test version issue

---
+++ src/test/scala/RegexTest.scala
@@ -0,0 +1,22 @@
+import org.scalatest._
+
+class RegexTest extends WordSpec with Matchers {
+  "A regex evaluator" should {
+    "match these basic regexes" in {
+      Regex.fullMatch("ab", "ab") should be(true)
+      Regex.fullMatch("abbbbb", "ab+") should be(true)
+      Regex.fullMatch("bbbbb", "ab+") should be(true)
+      Regex.fullMatch("aaabbb", "a+b+") should be(true)
+      Regex.fullMatch("ababa", "a+b+") should be(false)
+      Regex.fullMatch("aaa", "a*") should be(true)
+      Regex.fullMatch("", "a*") should be(true)
+    }
+
+    "match the . character properly" in {
+      Regex.fullMatch("a", ".") should be(true)
+      Regex.fullMatch("ab", ".") should be(false)
+      Regex.fullMatch("abab", "(..)*") should be(true)
+      Regex.fullMatch("aba", "(..)*") should be(false)
+    }
+  }
+}
